bdf8a7e89141a879d44ef73256e99161c4da162a
Paul Thomas <paulthomas2107@gmail.com>
2023-07-28 09:56:32 +0100

    More

---
+++ 01-variable-data-types/01-console/script.js
@@ -0,0 +1,18 @@
+console.log('Hello Paul');
+console.log(20, 'More', false);
+const x = 100;
+console.log(x);
+
+console.log('use clg'); // clg
+
+console.error('Oh !!');
+console.table({ name: 'Paul', email: 'ppp@ppp.com' });
+
+console.group('SIMPLE');
+console.log('xxxx');
+console.error('Alrt');
+console.warn('warning');
+console.groupEnd();
+
+const styles = 'padding: 10px; background-color: white;color: green;'
+console.log('%cHello Word', styles)
