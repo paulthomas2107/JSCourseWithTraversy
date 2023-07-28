62f7c67c3ab64df0871a5f0464089939acadaf99
Paul Thomas <paulthomas2107@gmail.com>
2023-07-28 14:24:00 +0100

    More

--- 03-variables-constants/script.js
+++ 03-variables-constants/script.js
@@ -21,10 +21,34 @@ console.log(age);
 
 // Reassigning
 age = 31;
-console.log(age)
+console.log(age);
 
 let score;
-score = 1
+score = 1;
 score++;
-console.log(score)
+console.log(score);
+
+const drive = 1233;
+console.log(drive);
+// const drive2; not allowed - must be set
+// drive++ not allowed
+console.log(drive);
+
+const arr1 = [1, 2, 3, 4];
+arr1.push(5);
+console.log(arr1);
+arr1.pop();
+console.log(arr1);
+
+const person = {
+  name: 'Paul',
+};
+
+console.log(person)
+person.name = 'John' // ok
+//person = 'Ken' // not allowed
+console.log(person); 
+person.email = 'p@p.com' //ok
+console.log(person);
+
 
---
+++ 04-datatypes/script.js
@@ -0,0 +1,39 @@
+// BigInt
+const x = BigInt(123_456_789_101_123);
+console.log(x);
+
+// String
+const firstName = 'Sarah';
+const output = firstName;
+const age = 30;
+const temp = 98.8;
+console.log(output, typeof firstName, age, temp, typeof temp);
+
+// Boolean
+const hasKids = true;
+console.log(hasKids, typeof hasKids);
+
+// Undefined
+let score;
+console.log(score, typeof score)
+const h1 = undefined;
+console.log(h1)
+
+// Symbol
+const id = Symbol('id9999') 
+const id1 = id;
+console.log(id1, typeof id1)
+
+// More BigInt
+const n = BigInt(99999999999n);
+console.log(n, typeof n)
+
+// Reference Types
+const numbers = [1, 2, 3, 5]
+console.log(numbers, typeof numbers)
+const p2 = {
+    'name': 'paul',
+    'age': 222,
+    'isHome': true
+}
+console.log(p2, typeof p2)
