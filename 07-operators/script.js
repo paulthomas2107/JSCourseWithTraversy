8fedae390d6bce642e332b6485b6b6354835e555
Paul Thomas <paulthomas2107@gmail.com>
2023-07-30 13:40:28 +0100

    More

---
+++ 07-operators/script.js
@@ -0,0 +1,89 @@
+// 1. Artithmetic operators
+let x;
+x = 5
+console.log(x)
+x = 5;
+x = 5 * 5
+console.log(x)
+x = 5;
+x = 5 + 5;
+console.log(x);
+x = 5;
+x = 5 - 5;
+console.log(x); 
+x = 5;
+x = 5 /  5;
+console.log(x);
+x = 5;
+x = 5 % 5;
+console.log(x);
+x = 5;
+x = 7 % 5;
+console.log(x);
+
+
+// Concatenation
+x = 'Hello ' + 'Paul'
+console.log(x);
+
+// Exponent
+x = 2 ** 4
+console.log(x);
+
+// Increment
+x = 10
+x++
+console.log(x);
+
+// Decrement
+x = 20
+x--
+console.log(x);
+
+// Assignment
+x = 10
+x += 5
+console.log(x);
+x *= 10
+console.log(x);
+x /= 10;
+console.log(x);
+x %= 10;
+console.log(x);
+x **= 10;
+console.log(x);
+
+// Comparison
+x = 2
+if (x == 2) {
+    console.log(x == 2)
+}
+x = 2 == 2
+console.log((x = 2 == 2));
+x = 2 === '2';
+console.log((x = 2 === '2')); // always use === triple =
+
+x = 2 != 2
+console.log(x)
+
+x = 2 != '2';
+console.log(x);
+
+x = 2 !== '2';
+console.log(x);
+
+x = 10 > 5;
+console.log(x);
+
+x = 10 < 5;
+console.log(x);
+
+x = 10 <= 5;
+console.log(x);
+
+x = 10 >= 5;
+console.log(x);
+
+
+
+
