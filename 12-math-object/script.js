6c8f3c6398f95a7c3e538a924e7dccc9d8d81c5c
Paul Thomas <paulthomas2107@gmail.com>
2023-07-30 16:15:21 +0100

    More

---
+++ 12-math-object/script.js
@@ -0,0 +1,42 @@
+console.log(Math);
+
+let x;
+
+x = Math.sqrt(9);
+console.log(x);
+
+x = Math.abs(-5);
+console.log(x);
+
+x = Math.round(9.6);
+console.log(x);
+
+x = Math.round(9.4);
+console.log(x);
+
+x = Math.ceil(9.6);
+console.log(x);
+
+x = Math.floor(9.6);
+console.log(x);
+
+x = Math.pow(2, 4);
+console.log(x);
+
+x = Math.min(800, 222, 333);
+console.log(x);
+
+x = Math.max(800, 222, 333);
+console.log(x);
+
+x = Math.random();
+console.log(x);
+
+x = Math.random() * 10;
+console.log(x);
+
+x = Math.random() * 10 + 1;
+console.log(x);
+
+x = Math.floor(Math.random() * 100 + 1);
+console.log(x);
---
+++ 13-number-challenge/script.js
@@ -0,0 +1,8 @@
+const x = Math.floor(Math.random() * 100 + 1);
+const y = Math.floor(Math.random() * 50 + 1);
+
+console.log(`Sum:  ${x} + ${y} = ${x + y}`)
+console.log(`Diff: ${x} - ${y} = ${x - y}`);
+console.log(`Prod: ${x} * ${y} = ${x * y}`);
+console.log(`Quot: ${x} / ${y} = ${x / y}`);
+console.log(`Rem:  ${x} % ${y} = ${x % y}`);
