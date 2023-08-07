7d792d9a44795a5d5c01580b354ce6230cbd5e6e
Paul Thomas <paulthomas2107@gmail.com>
2023-08-07 12:23:32 +0100

    More

--- 43-DOM/script.js
+++ 43-DOM/script.js
@@ -1,1 +1,18 @@
+console.log(window.document);
+console.dir(window.document);
+console.log(document.body.innerHTML);
+console.log(document.body.innerText);
 
+console.log(document.links[0]);
+
+//document.body.innerHTML = '<h1>Hello Paul</h1>';
+
+// Writes to screen - not used much
+//document.write('Hello from JS')
+
+console.log(document.getElementById('main'));
+const main = document.getElementById('main');
+//main.innerHTML = '<h1>Hello from Main</h1>'
+//document.querySelector('#main h1').innerText = 'Hello'
+
+// Document Element Properties
