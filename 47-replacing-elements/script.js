180db31878b9929b53fa2998c6287297686474b0
Paul Thomas <paulthomas2107@gmail.com>
2023-08-11 13:10:44 +0100

    More

--- 47-replacing-elements/script.js
+++ 47-replacing-elements/script.js
@@ -1,1 +1,23 @@
+function replaceFirstItem() {
+  const firstIitem = document.querySelector('li:first-child');
+  const li = document.createElement('li');
+  li.textContent = 'Replaced First';
+  firstIitem.replaceWith(li);
+}
 
+function replaceSecondItem() {
+  const secondItem = document.querySelector('li:nth-child(2)');
+  secondItem.outerHTML = '<li>Replaced Second</li>';
+}
+
+function replaceAllItems() {
+  const lis = document.querySelectorAll('li');
+  lis.forEach((item, index) => {
+    //item.outerHTML = `<li>Replace all - Item ${index + 1}</li>`;
+    item.innerHTML = `Replace all - Item ${index + 1}...`;
+  });
+}
+
+replaceFirstItem();
+replaceSecondItem();
+replaceAllItems();
