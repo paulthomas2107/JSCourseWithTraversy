0a834036c06d03b1d6446a48c74e36c740255fee
Paul Thomas <paulthomas2107@gmail.com>
2023-08-13 11:13:16 +0100

    More

--- 50-events/script.js
+++ 50-events/script.js
@@ -1,1 +1,23 @@
+// Event - when something happens !
+function onClear() {
+  console.log('Clear Items');
+}
 
+// JS Event Listener
+/*
+const clearBtn = document.querySelector('#clear'); // #... = id
+clearBtn.onclick = function () {
+  alert('Hi, Paul ! - Clear Items');
+};
+*/
+
+// Best way = addEventListener
+const clearBtn = document.querySelector('#clear'); // #... = id
+clearBtn.addEventListener('click', () => alert('Back...clear items')); // alert blocks...
+clearBtn.addEventListener('click', onClear); // Both work parenthesis required
+
+// Remove event listener after 5 secs
+//setTimeout(() => clearBtn.removeEventListener('click', onClear), 5000);
+
+// Autoclicks after 5 seconds
+setTimeout(() => clearBtn.click(), 5000);
