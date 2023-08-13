473b777e90b4ec6280bd26a4a5c909e8efadc640
Paul Thomas <paulthomas2107@gmail.com>
2023-08-13 17:33:45 +0100

    More

--- 53-keyboard-events/script.js
+++ 53-keyboard-events/script.js
@@ -10,11 +10,35 @@ const onKeyUp = (e) => {
 
 const onKeyDown = (e) => {
   // key
-  console.log(e.key);
+  //console.log(e.key);
+  if (e.key === 'Enter') {
+    alert('Enter pressed');
+  }
   // keyCode
-  console.log(e.keyCode);
+  //console.log(e.keyCode);
+  if (e.keyCode === 13) {
+    alert('Enter 13 pressed');
+  }
   // code
-  console.log(e.code);
+  //console.log(e.code);
+  if (e.code === 'Digit1') {
+    alert('Enter Digit 1 pressed');
+  }
+
+  if (e.repeat) {
+    console.log('Holding key down ' + e.key);
+  }
+  document.querySelector('h1').innerText = `${e.key} ${e.keyCode} ${e.code}`;
+
+  // Alt, ctrl, shift keys
+  console.log('Shift ' + e.shiftKey);
+  console.log('Ctrl ' + e.ctrlKey);
+  console.log('Alt ' + e.altKey);
+
+  // two keys shift etc
+  if (e.shiftKey && e.key === 'K') {
+    console.log('SHIFT K');
+  }
 };
 
 itemInput.addEventListener('keypress', onKeyPress);
