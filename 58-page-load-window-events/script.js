2ee283fadee968c626ab7b5e4a423838b63a875d
Paul Thomas <paulthomas2107@gmail.com>
2023-08-14 14:24:30 +0100

    More

--- 58-page-load-window-events/script.js
+++ 58-page-load-window-events/script.js
@@ -18,3 +18,22 @@ window.addEventListener('resize', () => {
     'h1'
   ).innerText = `Resized to ${window.innerWidth} x ${window.innerHeight}`;
 });
+
+window.addEventListener('scroll', () => {
+  console.log(`Scrolled ${window.scrollX} x ${window.scrollY}`);
+  if (window.scrollY > 70) {
+    document.body.style.backgroundColor = 'black';
+    document.body.style.color = 'white';
+  } else {
+    document.body.style.backgroundColor = 'white';
+    document.body.style.color = 'black';
+  }
+});
+
+window.addEventListener('focus', () => {
+  document.querySelectorAll('p').forEach((p) => (p.style.color = 'blue'));
+});
+
+window.addEventListener('blur', () => {
+  document.querySelectorAll('p').forEach((p) => (p.style.color = 'black'));
+});
